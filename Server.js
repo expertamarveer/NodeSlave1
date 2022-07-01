23ce496aa41f22c4d505ec0214cf3c87a5a519d3
expertamarveer <expert.student24@gmail.com>
2022-07-01 08:16:36 +0530

    adding express

---
+++ Server.js
@@ -0,0 +1,22 @@
+'use strict';
+require("appdynamics").profile({
+  controllerHostName: 'siemensil.saas.appdynamics.com',
+  controllerPort: 443,
+  controllerSslEnabled: true,  // Set to true if controllerPort is SSL
+  accountName: 'siemensil',
+  accountAccessKey: '2ili1qt6b0eu', //required
+  applicationName: 'agent',
+  tierName: 'yairagent',
+  nodeName: 'yairagent'
+ });
+const express = require('express');
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
