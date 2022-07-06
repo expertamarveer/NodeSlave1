9562c849efa181172e1e8f10ec5c081221c4ace5
expertamarveer <expert.student24@gmail.com>
2022-07-06 13:41:57 +0530

    change new

--- Server.js
+++ Server.js
@@ -20,9 +20,9 @@ const HOST = '127.0.0.1';
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
-  res.end();
+  //res.end();
+  
 });
 app.listen(PORT, HOST);
-
 //console.log('Running on http://${HOST}:${PORT}');
 console.log(`Running on http://${HOST}:${PORT}`);
