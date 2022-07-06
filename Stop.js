282abc5671ac71f14239cd185fddb7c1c2c571a1
expertamarveer <expert.student24@gmail.com>
2022-07-06 16:21:29 +0530

    change new

--- Stop.js
+++ Stop.js
@@ -1,11 +1,13 @@
 
 const express = require('express');
+
 const PORT = 8181;
 //const HOST = '0.0.0.0';
 const HOST = '127.0.0.1';
 const app = express();
-app.HOST  = HOST;
-app.PORT  = PORT;
+app.set('port', process.env.PORT || 8181);
+//app.HOST  = HOST;
+//app.PORT  = PORT;
 app.close();
-process.exit();
+//process.exit();
  
