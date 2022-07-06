b0ca8b567e1a08eed01f2860b7d2646c012d22cc
expertamarveer <expert.student24@gmail.com>
2022-07-06 16:31:45 +0530

    change new

--- Stop.js
+++ Stop.js
@@ -1,12 +1,4 @@
 
-const express = require('express');
-const PORT = 8181;
-//const HOST = '0.0.0.0';
-const HOST = '127.0.0.1';
-const app = express();
-app.set('port', process.env.PORT || 8181);
-//app.HOST  = HOST;
-//app.PORT  = PORT;
-app.close();
-//process.exit();
+const kill = require("kill-port");
+kill(8181, "tcp");
  
